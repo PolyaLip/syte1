9b61a09abbfea20a9df8bb0d48549ddb789d2bac
PolyaLip <144261383+PolyaLip@users.noreply.github.com>
2023-10-21 16:15:14 +0300

    Update new.js

--- new.js
+++ new.js
@@ -21,14 +21,10 @@ function verify() {
 }
 
 function send() {
-    if (check) {
         let textCondition = document.getElementsByTagName('p')[0].innerText
         document.getElementsByName('formulation')[0].value = textCondition;
         document.getElementsByName('result')[0].value = result;
         document.getElementById("UserEnter").submit();
-    } else {
-        alert("Есть недостатки. Повторите ввод")
-    }
 }
 
 function verify_send() {
